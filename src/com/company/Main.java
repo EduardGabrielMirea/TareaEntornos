929e8b302b8fcca7e0777f2c5593774186e732bc
Eduard Gabriel Mirea <eduardmirea00@gmail.com>
2024-02-14 12:08:33 +0100

    update3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,18 +6,24 @@ public class Main {
         Scanner teclado=new Scanner(System.in);
         System.out.println("Introduce el número para la criba de Erastótenes:");
 
-        int dato = teclado.nextInt();
-        int vector[] = new int[dato];
+        //Pedimos un dato al usuario en forma de 1 numero entero, que usaremos para la medida de nuestro vector.
+        int medidaVector = teclado.nextInt();
+        int vector[] = new int[medidaVector];
 
-        System.out.println("\nVector inicial hasta :"+dato);
+        //Nos genera el vector con 100 posiciones empezando desde 0[0] a 99[100] y lo imprime en lineas de 10 numeros.
+        System.out.println("\nVector inicial hasta :"+medidaVector);
         leerDatos(vector);
 
-        vector=generarPrimos(dato);
-        System.out.println("\nVector de primos hasta:"+dato);
+        //Reecribimos nuestro vector con el resultado que nos a dado el metodo generarPrimos.
+        vector = generarPrimos(medidaVector);
+        System.out.println("\n");
 
-        buscarPrimos(vector);
+        // Imprime el vector que esta lleno de numeros primos, de forma en que en cada linea solo haya 10 numeros por linea, junto con un tabluador para separarlos.
+        System.out.println("\nVector de primos hasta:"+medidaVector);
+        imprimirLosPrimos(vector);
     }
 
+    //Nos genera el vector con 100 posiciones empezando desde 0[0] a 99[100] y lo imprime en lineas de 10 numeros.
     public static void leerDatos(int[] vector) {
         for (int i = 0; i < vector.length; i++) {
             if (i%10==0) System.out.println();
@@ -25,64 +31,13 @@ public class Main {
         }
     }
 
-    public static void buscarPrimos(int[] vector) {
-        for (int i = 0; i < vector.length; i++) {
-            if (i%10==0) System.out.println();
-            System.out.print(vector[i]+"\t");
-        }
-    }
-
-    // Inicializar el array
-    public static void inicializar(int dimension, boolean[] esPrimo) {
-        int i;
-        for (i=0; i< dimension; i++)
-            esPrimo[i] = true;
-    }
-
-    // Eliminar el 0 y el 1, que no son primos
-    public static void eliminar(boolean[] esPrimo) {
-        esPrimo[0] = esPrimo[1] = false;
-    }
-
-    private static int getCuenta(int dimension, boolean[] esPrimo) {
-        int i;
-        int cuenta = 0;
-        for (i=0; i< dimension; i++) {
-            if (esPrimo[i])
-                cuenta++;
-        }
-        return cuenta;
-    }
-
-
-    public static void multiplosDeI(int dimension, boolean[] esPrimo) {
-        int j;
-        int i;
-        for (i=2; i<Math.sqrt(dimension)+1; i++) {
-            if (esPrimo[i]) {
-                // Eliminar los múltiplos de i
-                for (j=2*i; j< dimension; j+=i)
-                    esPrimo[j] = false;
-            }
-        }
-    }
-
-    private static int[] getInts(int cuenta, int dimension, boolean[] esPrimo) {
-        int j;
-        int i;
-        int[] primos = new int[cuenta];
-        for (i=0, j=0; i< dimension; i++) {
-            if (esPrimo[i])
-                primos[j++] = i;
-        }
-        return primos;
-    }
-
-    // Generar números primos de 1 a dato
-    public static int[] generarPrimos (int dato){
+    public static int[] generarPrimos (int medidaVector){
         int i,j;
-        if (dato >= 2) {
-            int dimension = dato + 1; // Tamaño del array
+
+        //El if nos obliga a meter una medidaVector por Scanner que sea 2 o mayor que 2. En otro caso el programa solo nos devol.
+        if (medidaVector >= 2) {
+            int dimension = medidaVector + 1; // Tamaño del array
+            //Pasamos cada posicion del vector a no es primo.
             boolean[] esPrimo = new boolean[dimension];
 
             // Inicializar el array
@@ -102,9 +57,64 @@ public class Main {
 
             return primos;
 
-        } else { // dato < 2
+        } else { // medidaVector < 2
             return new int[0];
-    // Vector vacío
+            // Vector vacío
+        }
+    }
+
+    // Inicializar el array
+    public static void inicializar(int dimension, boolean[] esPrimo) {
+        int i;
+        for (i=0; i< dimension; i++)
+            esPrimo[i] = true;
+    }
+
+    // Eliminar el 0 y el 1, que no son primos
+    public static void eliminar(boolean[] esPrimo) {
+        esPrimo[0] = esPrimo[1] = false;
+    }
+    // Eliminar los múltiplos de 2 recoriendo el vector de 2 en 2 posiciones y pasandolos a false usando el bucle for j.
+    public static void multiplosDeI(int dimension, boolean[] esPrimo) {
+        int j;
+        int i;
+        for (i=2; i<Math.sqrt(dimension)+1; i++) {
+            if (esPrimo[i]) {
+                //Cojemos el vector y lo recorremos en j+=i que quiere decir de 2 en 2 ya que igualamos j+ a i donde i=2. Pasamos las posiciones a false. ej: 10 = false, 12= false, etc.
+                for (j=2*i; j< dimension; j+=i)
+                    esPrimo[j] = false;
+            }
+        }
+    }
+
+    // ¿Cuántos primos hay?
+    public static int getCuenta(int dimension, boolean[] esPrimo) {
+        int i;
+        int cuenta = 0;
+        for (i=0; i< dimension; i++) {
+            if (esPrimo[i])
+                cuenta++;
+        }
+        return cuenta;
+    }
+
+    // Rellenar el vector de números primos
+    public static int[] getInts(int cuenta, int dimension, boolean[] esPrimo) {
+        int j;
+        int i;
+        int[] primos = new int[cuenta];
+        for (i=0, j=0; i< dimension; i++) {
+            if (esPrimo[i])
+                primos[j++] = i;
+        }
+        return primos;
+    }
+
+    // Imprime el vector que esta lleno de numeros primos, de forma en que en cada linea solo haya 10 numeros por linea, junto con un tabluador para separarlos.
+    public static void imprimirLosPrimos(int[] vector) {
+        for (int i = 0; i < vector.length; i++) {
+            if (i%10==0) System.out.println();
+            System.out.print(vector[i]+"\t");
         }
     }
 }
